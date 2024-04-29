a6be9e1dde45cb5b6470dcf0e42de6e972f12692
Cam <112569387+p000hs@users.noreply.github.com>
2024-04-28 21:14:42 -0400

    commencement

---
+++ script/script.js
@@ -0,0 +1,40 @@
+//INDEX
+gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(TextPlugin);
+gsap.registerPlugin(MotionPathPlugin)
+
+const chapitre1 = document.querySelector('#chpt1');
+const chapitre2 = document.querySelector('#chpt2');
+const chapitre3 = document.querySelector('#chpt3');
+const chapitre4 = document.querySelector('#chpt4');
+const chapitre5 = document.querySelector('#chpt5');
+const chapitre6 = document.querySelector('#chpt6');
+const chapitre7 = document.querySelector('#chpt7');
+const chapitre8 = document.querySelector('#chpt8');
+const bodyIndex = document.querySelector('body');
+
+
+let timeline = gsap.timeline().from(['.fleche-down'], {
+    duration: 0.8,
+    opacity: 0,
+    repeat: -1,
+    ease: 'slow',
+    stagger: {
+        each: 0.5,
+    },
+})
+
+
+const techniqueGsap = document.querySelector('.technique-gsap');
+let scrollIndex;
+if (techniqueGsap != null) {
+    window.addEventListener('scroll', () => {
+        bodyIndex.classList.add('is-scrolling');
+        window.clearTimeout(scrollIndex);
+        scrollIndex = setTimeout(removeClassIsScrolling, 100);
+    });
+
+    function removeClassIsScrolling() {
+        bodyIndex.classList.remove('is-scrolling');
+    }
+}
